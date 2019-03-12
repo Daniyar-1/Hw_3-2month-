b5e054673d8075a6bd50ef5de90b269218b61afc
Daniyar <daniyar01.m@gmail.com>
2019-03-12 13:56:00 +0600

    Second commit

---
+++ src/BankAccount.java
@@ -0,0 +1,20 @@
+public class BankAccount {
+    double amount;
+
+    public double getAmount() {
+            return amount;
+    }
+
+    public double deposit(double som) {
+       return amount=amount+som;
+
+    }
+
+        public double withDraw(double sum)throws LimitException{
+        if(sum > amount ){
+            throw new LimitException("Запращиваемая сумма на снятие больше чем остаток на счёте");
+        }
+        return amount=amount-sum;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) throws LimitException {
+
+        BankAccount bankAccount = new BankAccount();
+
+        bankAccount.deposit(10000);
+
+
+        while (true) {
+            try {
+                if (bankAccount.getAmount() > 6000) {
+                    bankAccount.withDraw(6000);
+                } else {
+                    throw new LimitException();
+                }
+            } catch (LimitException q) {
+                bankAccount.withDraw(bankAccount.amount);
+                q.printStackTrace();
+                break;
+            }
+
+        }
+
+
+    }
+}
+
+
+
